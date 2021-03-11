25c2c5e7dc8de8b6048a91eb821f53d4072b60c3
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-03-11 14:35:51 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let prev;
+  let ind = [];
+  do{
+    prev = arr.length;
+    ind = [];
+  bracketsConfig.forEach(pair => {
+      arr.forEach((bracket,index,a) => {
+      if (pair[0] == bracket && pair[1] == a[index + 1]) {
+        ind.push(index);
+        ind.push(index + 1);
+      }
+    })
+  });
+  arr = arr.filter((e,i) => !ind.includes(i));
+}while (arr.length < prev)
+  return arr.length == 0 ;
 }
